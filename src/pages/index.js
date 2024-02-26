e65aab24be66447709eb891d7307f8252858894b
tkozuch <kozuch.tomasz1@gmail.com>
2024-02-27 00:28:02 +0100

    initial test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulationsss
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
