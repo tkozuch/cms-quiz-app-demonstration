4fac909ea7303498f3bdeea8457446979925d386
tkozuch <kozuch.tomasz1@gmail.com>
2024-03-13 22:54:15 +0100

    index page layout -- working very good

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,29 +13,37 @@ const IndexPage = ({
     <Layout>
       <img src={logo} alt="Logo" />
       <ul
-        className="flex flex-wrap w-full gap-x-[--gapx] gap-y-8 max-h-[70vh] overflow-y-auto items-center break-words py-[10%] sm:py-0
-        [--mask:linear-gradient(0deg,#0000,#000_10%_90%,#0000)] sm:[--mask:none] [-webkit-mask:--mask] [mask:--mask] "
+        className="flex flex-wrap w-full gap-x-[--gapx] gap-y-8 overflow-y-auto items-center break-words py-[7%] md:py-0
+        [--mask:linear-gradient(0deg,#0000,#000_4%_96%,#0000)] md:[--mask:none] [-webkit-mask:--mask] [mask:--mask] mt-4 md:mt-16 grow content-normal md:mb-8"
         style={{
           "--gapx": "4rem",
         }}
       >
-        {allMarkdownRemark.nodes.map((quiz) => (
-          <li
-            key={quiz.gatsbyPath}
-            className="flex flex-col flex-wrap w-full md:w-[--md-width] lg:w-[--lg-width] text-center"
-            style={{
-              "--lg-width": "calc(25% - var(--gapx))",
-              "--md-width": "calc(33.3333% - var(--gapx))",
-            }}
-          >
-            <Link
-              to={quiz.gatsbyPath}
-              className="text-xl max-w-full text-green-900 underline underline-offset-2 hover:text-green-700 transition-[color]"
+        {allMarkdownRemark.nodes
+          // test more items
+          // .reduce((p, c) => {
+          //   p.push(c, c, c);
+          //   return p;
+          // }, [])
+          // test fewer items
+          // .slice(0, 3)
+          .map((quiz) => (
+            <li
+              key={quiz.gatsbyPath}
+              className="flex flex-col flex-wrap w-full md:w-[--md-width] lg:w-[--lg-width] text-center"
+              style={{
+                "--lg-width": "calc(25% - var(--gapx))",
+                "--md-width": "calc(33.3333% - var(--gapx))",
+              }}
             >
-              {quiz.frontmatter.title}
-            </Link>
-          </li>
-        ))}
+              <Link
+                to={quiz.gatsbyPath}
+                className="text-xl max-w-full text-green-900 underline underline-offset-2 hover:text-green-700 transition-[color]"
+              >
+                {quiz.frontmatter.title}
+              </Link>
+            </li>
+          ))}
       </ul>
     </Layout>
   );
